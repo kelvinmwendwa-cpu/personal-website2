1a46359bed3083ffb6827fb48297731a231dd853
kelvinmwendwa-cpu <mwendwak123@gmail.com>
2025-03-18 20:31:27 +0300

    Ensure dotenv is required in server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 require("dotenv").config(); // Load environment variables from .env file
 const express = require("express");
 const bodyParser = require("body-parser");
